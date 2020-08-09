c5ae8f9c72d495719f7d56a6bf123c2f21d343d5
Dheeraj <dheerajdagar94@gmail.com>
2020-08-09 20:46:11 +0530

    Fixed issue of multiple tabs opening on search

--- js/search.js
+++ js/search.js
@@ -9,7 +9,7 @@ chrome.contextMenus.create(
 	{
 		title: "google", 
 		contexts:["selection"], 
-		onclick: searchSelection,
+		onclick: searchSelection1,
 		parentId: "parent",
 	}
 );
@@ -17,7 +17,7 @@ chrome.contextMenus.create(
 	{
 		title: "yahoo", 
 		contexts:["selection"], 
-		onclick: searchSelection,
+		onclick: searchSelection2,
 		parentId: "parent",
 	}
 );
@@ -25,7 +25,7 @@ chrome.contextMenus.create(
 	{
 		title: "bing", 
 		contexts:["selection"], 
-		onclick: searchSelection,
+		onclick: searchSelection3,
 		parentId: "parent",
 	}
 );
@@ -33,7 +33,7 @@ chrome.contextMenus.create(
 	{
 		title: "wiki", 
 		contexts:["selection"], 
-		onclick: searchSelection,
+		onclick: searchSelection4,
 		parentId: "parent",
 	}
 );
@@ -41,16 +41,28 @@ chrome.contextMenus.create(
 	{
 		title: "Amazon", 
 		contexts:["selection"], 
-		onclick: searchSelection,
+		onclick: searchSelection5,
 		parentId: "parent",
 	}
 );
 
-function searchSelection(info) {
+function searchSelection1(info) {
 	var searchString = info.selectionText
 	chrome.tabs.create({url: "https://www.google.com/search?q=" + searchString})
+}
+function searchSelection2(info) {
+	var searchString = info.selectionText
 	chrome.tabs.create({url: "https://www.bing.com/search?q=" + searchString})
+}
+function searchSelection3(info) {
+	var searchString = info.selectionText
 	chrome.tabs.create({url: "https://search.yahoo.com/search?vc=&p=" + searchString})
+}
+function searchSelection4(info) {
+	var searchString = info.selectionText
 	chrome.tabs.create({url: "http://en.wikipedia.org/wiki/Special:Search?search=" + searchString+ "&go=Go"})
+}
+function searchSelection5(info) {
+	var searchString = info.selectionText
 	chrome.tabs.create({url: "https://www.amazon.in/s?k=" + searchString})
 }
